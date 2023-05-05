2e9ddbfd77f76a41a09666f27c7612dc99d9fe7f
Ja Navey <jsnavey@gmail.com>
2023-05-04 21:38:35 -0400

    change onclick function to addEventListener

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-
-// Testing if the js script is linked.
+// Open and close nav bar in mobile size screen
 
 const hamburger = document.querySelector(".fi-br-menu-burger");
 const cross = document.querySelector(".fi-br-cross");
 const navLists = document.querySelector(".nav-lists");
 
+/*
 hamburger.onclick = function() {
     navLists.classList.toggle("active");
 };
@@ -12,6 +12,14 @@ hamburger.onclick = function() {
 cross.onclick = function() {
     navLists.classList.remove("active");
 };
+*/
 
+hamburger.addEventListener("click", () => {
+    navLists.classList.toggle("active");
+});
+
+cross.addEventListener("click", () => {
+    navLists.classList.remove("active");
+});
 
 
